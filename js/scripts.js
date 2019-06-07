931e81d8cfe1a8fa49e57b90cc46feff049df3c1
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-07 10:34:04 -0700

    add additional form questions, add variables to store data

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  $("#survey").submit(function() {
-
+  $("#survey").submit(function(event) {
+    var userAge = parseInt($("#age").val());
+    var userExperience = parseInt($("#experience").val());
+    var userEnjoy = $("#enjoy").val();
+    var userColor = $("#color").val();
+    var userSemicolons = $("#semicolons").val();
+    event.preventDefault();
   })
 });
